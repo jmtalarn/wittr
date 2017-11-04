13442ff45d5775e706843d8f3c5886eb209895c6
Joan Maria Talarn Espelta <jmtalarn@gmail.com>
2017-11-04 01:19:40 +0100

    Hijacking requests 1

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,6 +1,13 @@
-self.addEventListener('fetch', function(event) {
-	// TODO: respond to all requests with an html response
-	// containing an element with class="a-winner-is-me".
-	// Ensure the Content-Type of the response is "text/html"
+self.addEventListener('fetch', function (event) {
+  // TODO: respond to all requests with an html response
+  // containing an element with class="a-winner-is-me".
+  // Ensure the Content-Type of the response is "text/html"
   console.log(event.request);
+  event.respondWith(
+    new Response('<div class="a-winner-is-me">Hero!</div>', {
+      headers: {
+        "Content-Type": "text/html"
+      }
+    })
+  );
 });
